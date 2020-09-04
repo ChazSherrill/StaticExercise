31d3d598042cf09ca818b93de86bc1a8ffa91f01
Chaz Sherrill <chazsherrill@icloud.com>
2020-09-04 10:28:41 -0500

    static exercise answer

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(TempConverter.CelsiusToFahrenheit(50m));
+
+            Console.WriteLine(TempConverter.FahrenheitToCelsius(50m));
+        }
+
+    }
+}
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+        public static decimal FahrenheitToCelsius(decimal fahren)
+        {
+            return Decimal.Divide(((fahren - 32) * 5) , 9);
+        }
+
+        public static decimal CelsiusToFahrenheit(decimal cels)
+        { 
+            return (cels * Decimal.Divide(9 , 5)) + 30;
+        }
+    }
+}
